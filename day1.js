d670252ad7df8771335d9bcc531b73f129d27a03
sanjit961 <sanjitlungeli241@gmail.com>
2023-08-25 19:30:03 +0530

    day 01

---
+++ day1.js
@@ -0,0 +1,176 @@
+// Day 01
+
+// functions
+
+// function helo() {
+//   return "Heloo this is a test!";
+// }
+
+// const test = helo();
+
+// console.log(test);
+
+// function add(txt) {
+//   return "Helo: " + txt;
+// }
+
+// const name = add("My name is Krishna!");
+// console.log(name);
+
+// function add(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+// const sum = add(12, 12, 4);
+
+// function multiply(num1, num2, num3) {
+//   return num1 * num2 * num3;
+// }
+
+// const product = multiply(12, 12, 4);
+
+// console.log(product);
+
+// Quotient
+// function divide(num1, num2) {
+//   return num1 / num2;
+// }
+// const result = divide(10, 5);
+// console.log(result);
+
+// Remainder
+
+// function remainder(num2, num1) {
+//   return num2 % num1;
+// }
+
+// const remains = remainder(10, 5);
+// console.log(remains);
+
+// let
+// const
+// var
+
+// let
+// let and const - local scope
+// let can be redeclared
+// const cannot be redeclared
+// var - global scope
+
+// let myName = "sanjit";
+
+// {
+//   myName = "Krishna";
+//   console.log(myName);
+// }
+
+// console.log(myName);
+
+// const yourName = "don";
+
+// {
+//   const yourName = "sanjit";
+//   console.log(yourName);
+// }
+
+// console.log(yourName);
+
+// var friendName = "Akshay Kumar";
+
+// {
+//   var friendName = "Subham";
+//   // let myName = "sanjit";
+//   // Error
+// }
+
+// console.log(myName); error dot defined
+// console.log(friendName);
+
+// var friends;
+
+// {
+//   friends = ["Subham", "Sanjit", "Krish"];
+// }
+
+// console.log(friends);
+
+// Array - collection of data types
+
+// let myArr = ["Akm", "bkm", "bfg", "salman"];
+
+// console.log(myArr);
+
+// Array length
+
+// let arrlength = myArr.length;
+// console.log(arrlength);
+
+// for (let i = 0; i < myArr.length; i++) {
+//   console.log(myArr[i]);
+// }
+
+// Pop - removes the last item
+
+// myArr.pop();
+// myArr.shift();
+
+// console.log(myArr);
+
+// Slice
+// let myArr2 = ["Akm", "bkm", "bfg", "salman"];
+
+// const slicedItem = myArr2.slice(0, 2);
+
+// const slicedItem = myArr2.slice(-2, -1);
+
+// console.log(slicedItem);
+
+// console.log(myArr2);
+
+// Reverse
+
+// const reversed = myArr2.reverse();
+// console.log(reversed);
+
+// Push
+
+// myArr2.push("carryminati");
+// console.log(myArr2);
+
+// map
+
+// myArr2.map((item, index) => {
+//   console.log("item", item, index);
+// });
+
+// myArr.map((data, key) => {
+//   console.log(data, key);
+// });
+
+// let fruits = ["apple", "orange", "grapes", "banana"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// Runs loops but shorter syntax
+
+// fruits.map((item) => console.log(item));
+
+// Tasks
+
+// 1) Multiplication table using for loop
+
+// 5 * 1 = 5
+// 5 * 2 = 10
+// 5 * 3 = 15
+// 5 * 4 = 20
+// 5 * 10 = 50
+
+// 2) using funtion print 
+// My Name is - Sanjit Lungeli
+// My pincode is - 676767
+// My Mobile number is - 98797937934
+
+// 3) Add items in Array - 2 items add, delete first two items - using map print
+// ["sdfd", "sdfsdf", "sfds", "sdfsf"]
+
