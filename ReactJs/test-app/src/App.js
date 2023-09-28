daa122c736a781bd250fee6fe1710e5aa606c522
sanjit961 <sanjitlungeli241@gmail.com>
2023-09-28 15:38:08 +0530

    React js props

--- ReactJs/test-app/src/App.js
+++ ReactJs/test-app/src/App.js
@@ -11,13 +11,29 @@ const App = () => {
   const [friendName, setFriendName] = useState("");
 
   let demoArr = [
-    { name: "test1" },
-    { name: "test2" },
-    { name: "test3" },
-    { name: "test4" },
-    { name: "test5" },
-    { name: "test5" },
+    { name: "test1", pin: "345353" },
+    { name: "test2", pin: "67744" },
+    { name: "test3", pin: "677447" },
+    { name: "test4", pin: "374477" },
+    { name: "test5", pin: "0777" },
+    { name: "test5", pin: "64777" },
   ];
+
+  const demoObj = {
+    name: "test",
+    number: "122",
+  };
+  const demoObj2 = {
+    name: "test1",
+    number: "1223",
+  };
+
+  const testFunct = (name) => {
+    console.log("I am a props fucntion", name);
+  };
+  const testFunct2 = (name) => {
+    console.log("Hello! ", name);
+  };
 
   return (
     <>
@@ -28,11 +44,24 @@ const App = () => {
           value={friendName}
           onChange={(e) => setFriendName(e.target.value)}
         />
-        <DemoProp name={friendName} address="Haryana" />
+        <DemoProp
+          name={friendName}
+          isChecked={false}
+          address="Haryana"
+          demoData={demoObj2}
+          testFunction={testFunct}
+        />
       </div>
       {/* <Sum /> */}
       <DemoTest />
-      <DemoProp name="test name" address="Assam" data={demoArr} />
+      <DemoProp
+        isChecked={true}
+        name="test name"
+        address="Assam"
+        data={demoArr}
+        demoData={demoObj}
+        testFunction={testFunct2}
+      />
 
       {/* <DemoInput /> */}
     </>
--- ReactJs/test-app/src/component/DemoProp.js
+++ ReactJs/test-app/src/component/DemoProp.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function DemoProp(props) {
   return (
-    <div>
-      <h2>Test Props</h2>
-      <p>Name: {props.name}</p>
-      <p>Address: {props.address}</p>
-      {props.data?.map((item) => (
-        <li>{item.name}</li>
-      ))}
-    </div>
+    <>
+      {props?.isChecked ? (
+        <div style={{ backgroundColor: props?.isChecked ? "red" : "yellow" }}>
+          <h2>Test Props</h2>
+          <p>Name: {props.name}</p>
+          <p>Address: {props.address}</p>
+          {props.data?.map((item) => (
+            <li>
+              Name: {item.name}, Pin: {item.pin}
+            </li>
+          ))}
+          <div>
+            <h2>User Demo</h2>
+
+            <p>Name: {props.demoData?.name}</p>
+            <p>Number: {props.demoData?.number}</p>
+            <button onClick={() => props?.testFunction("test")}>
+              Test click
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h3>No data!</h3>
+        </div>
+      )}
+    </>
   );
 }
 
